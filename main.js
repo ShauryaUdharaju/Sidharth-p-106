d071dd93083ea426ee35106faaa09b769f802b9d
ShauryaUdharaju <118858893+ShauryaUdharaju@users.noreply.github.com>
2024-08-19 20:08:50 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 //https://teachablemachine.withgoogle.com/models/GD2sHD8_a/
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/GD2sHD8_a/model.json", modelReady)
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+    console.log("model is loaded")
+}
+
+function gotResults(error, results)
+{
+    
+
+}
